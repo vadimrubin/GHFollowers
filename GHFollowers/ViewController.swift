bea92dd3e01659046b579f6272f45f20f66f7edc
vadimrubin <rubin.vadim.17@gmail.com>
2024-08-29 16:26:32 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Rubin Vadim on 29.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
