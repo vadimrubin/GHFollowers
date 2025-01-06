f1502ce5615c6350bb0ebd09c519c91b08c77b63
vadimrubin <rubin.vadim.17@gmail.com>
2025-01-06 16:25:29 +0300

    Jan 6, 2025 2nd commit

--- GHFollowers/Screens (Controllers)/UserInfoVC.swift
+++ GHFollowers/Screens (Controllers)/UserInfoVC.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SafariServices
 
 protocol UserInfoVCDelegate {
     func didTapGitHubProfile(for user: User)
@@ -130,10 +129,7 @@ extension UserInfoVC: UserInfoVCDelegate {
             presentGFAlertOnMainThread(title: "Invalid URL", message: "The URL attached to the user is invalid", buttonTitle: "Ok")
             return
         }
-        
-        let safariVC = SFSafariViewController(url: url)
-        safariVC.preferredControlTintColor = .systemGreen
-        present(safariVC, animated: true)
+        presentSafariVC(with: url)
     }
     
     func didTapGetFollowers(for user: User) {
